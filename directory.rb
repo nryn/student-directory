073c209479b3e53b0c5e68127f611cb911875387
nryn <narayanohanlon@gmail.com>
2017-02-03 15:58:45 +0000

    Exercise 7. setting all input possibilities to have default values, using symbols for cohort month, and ensures only full months can be typed

--- directory.rb
+++ directory.rb
@@ -1,3 +1,11 @@
+def defaulter(str) # function which gets user input and defaults it if empty
+  str = gets.chomp
+  if str.empty?
+    str = "None provided"
+  end
+  return str
+end
+
 def input_students
   puts "Please enter the name of a student"
   puts "To quit, just hit return twice."
@@ -8,13 +16,20 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
+    puts "Please enter this student's cohort."
+    months = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
+    cohort = defaulter(cohort).downcase.to_sym # this defaulter function does the gets.chomp and checks the default value in one go
+    unless months.include? cohort
+      puts "Please try again.\nEnter a month for the student's cohort"
+      cohort = defaulter(cohort).downcase.to_sym
+    end
     puts "Please enter this student's height."
-    height = gets.chomp
+    height = defaulter(height)
     puts "Please enter this student's favourite hobby."
-    hobbies = gets.chomp
+    hobbies = defaulter(hobbies)
     puts "Please enter this student's country of birth."
-    birthplace = gets.chomp
-    students << {name: name, cohort: :november, height: height, hobbies: hobbies, birthplace: birthplace}
+    birthplace = defaulter(birthplace)
+    students << {name: name, cohort: cohort, height: height, hobbies: hobbies, birthplace: birthplace}
     puts "Now we have #{students.count} students"
     # get another name from the user
     puts "Please enter another student's name"
@@ -37,10 +52,10 @@ def print(students)
       width = 50
       spacer = ' '
       puts "#{counter}. #{students[incrementer][:name]}"
-      puts " Originally from #{students[incrementer][:birthplace]} ".center(width,spacer)
+      puts " Birthplace: #{students[incrementer][:birthplace]} ".center(width,spacer)
       puts " Height: #{students[incrementer][:height]} ".center(width,spacer)
       puts " Hobbies: #{students[incrementer][:hobbies]} ".center(width,spacer)
-      puts " (#{students[incrementer][:cohort]} cohort) ".center(width,spacer)
+      puts " Cohort: #{students[incrementer][:cohort]} ".center(width,spacer)
       incrementer += 1
   end
 end
