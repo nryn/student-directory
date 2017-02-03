b52208acf2fe9382d592f038eb49a55c88fffe01
nryn <narayanohanlon@gmail.com>
2017-02-03 20:17:30 +0000

    now able to write student list to file

--- directory.rb
+++ directory.rb
@@ -6,6 +6,7 @@ $linespacer = ' '
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -22,6 +23,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -121,6 +124,18 @@ def show_students
   print_header
   print_students_by_month
   print_footer
+end
+
+def save_students
+  # open file
+  file = File.open("students.csv", "w")
+  # iterate over students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:height], student[:hobbies], student[:birthplace]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 # students = input_students
