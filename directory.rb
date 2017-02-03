1865bf4046154203d4fbe8d54d2a23806c46f671
nryn <narayanohanlon@gmail.com>
2017-02-03 19:37:55 +0000

    adding interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,25 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      printbymonth(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 $months = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
 $linewidth = 50
 $linespacer = ' '
@@ -90,10 +112,12 @@ def print_footer(students)
   puts "."
 end
 
-students = input_students
-if students.length != 0
-  print_header
-  puts "Ordered By Month:"
-  printbymonth(students)
-  print_footer(students)
-end
+# students = input_students
+# if students.length != 0
+#   print_header
+#   puts "Ordered By Month:"
+#   printbymonth(students)
+#   print_footer(students)
+# end
+
+interactive_menu
