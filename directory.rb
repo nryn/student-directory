cdf13c4a79eeef7a91fb0b90092941f99ad8f752
nryn <narayanohanlon@gmail.com>
2017-02-03 12:23:07 +0000

    refactored to use methods for printing each part of the document

--- directory.rb
+++ directory.rb
@@ -1,22 +1,33 @@
 # put students in to an array
- students= [
-   "Dr. Hannibal Lecter",
-   "Darth Vader",
-   "Nurse Ratched",
-   "Michael Corleone",
-   "Alex DeLarge",
-   "The Wicked Witch of the West",
-   "Terminator",
-   "Freddy Krueger",
-   "The Joker",
-   "Joffrey Baratheon",
-   "Norman Bates"
- ]
-# and print them
-puts "The students of Villains Academy"
-puts "----------"
-students.each do |student|
-  puts student
+students= [
+ "Dr. Hannibal Lecter",
+ "Darth Vader",
+ "Nurse Ratched",
+ "Michael Corleone",
+ "Alex DeLarge",
+ "The Wicked Witch of the West",
+ "Terminator",
+ "Freddy Krueger",
+ "The Joker",
+ "Joffrey Baratheon",
+ "Norman Bates"
+]
+
+def print_header
+   puts "The students of Villains Academy"
+   puts "----------"
 end
-# then we print total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# nothing happens 'til we call methods
+print_header
+print(students)
+print_footer(students)
