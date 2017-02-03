4d1801065386e8ba7dc75795931170960dbe40bf
nryn <narayanohanlon@gmail.com>
2017-02-03 13:09:54 +0000

    Exercise 2 - making the list print only names beginning with the letter 'S'

--- directory.rb
+++ directory.rb
@@ -24,7 +24,9 @@ end
 
 def print(students)
   students.each_with_index do |student,ind|
-    puts "#{ind+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if /^[Ss].*/ === student[:name]
+      puts "#{ind+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
