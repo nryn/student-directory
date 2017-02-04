18a14378bf33f560f7c5a35753d794bf31639bb2
nryn <narayanohanlon@gmail.com>
2017-02-04 13:19:39 +0000

    14. Exercise 2. Now loading students csv by default

--- directory.rb
+++ directory.rb
@@ -147,8 +147,10 @@ end
 
 def try_load_students
   filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
+  if filename.nil?
+    load_students
+    puts "Loaded #{@students.count} students."
+  elsif File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else
